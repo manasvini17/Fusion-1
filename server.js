bed7f4837d01b488613b64ee80f6c2d6824f811b
Sanmathi17 <144797310+Sanmathi17@users.noreply.github.com>
2024-07-03 11:12:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ app.get('/', (req, res) => {
 
 // Start the server and have it listen on the specified port
 app.listen(port, () => {
-  console.log(Server is running on http://localhost:${port});
+  console.log('Server is running on http://localhost:${port}');
 });
